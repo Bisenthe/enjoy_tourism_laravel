27a4c2c0ff449f1bfc27a35d88eb0c3d2585f266
bisenthe <nbisenthe@gmail.com>
2024-07-13 19:16:05 +0200

    fix:protection des routes

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\ClientController;
 
 Route::post('/login',[UserController::class,'login']);
 Route::post('/register',[UserController::class,'register']);
-Route::get('/user/all',[UserController::class,'index']);
-Route::get('/client/all',[ClientController::class,'index']);
+
 Route::group(['middleware'=>['auth:sanctum']], function () {
-    //
+    Route::get('/user/all',[UserController::class,'index']);
+    Route::get('/client/all',[ClientController::class,'index']);
     });
